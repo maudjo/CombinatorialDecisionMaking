09bebdeaf89cd1842e03afa55957b5d4c7acca19
Maud <johansson.maud98@gmail.com>
2022-05-18 18:35:55 +0200

    added instances

--- ConvertingToDzn.py
+++ ConvertingToDzn.py
@@ -1,20 +1,44 @@
 import pymzn;
 import pathlib;
 
-def ConvertToDzn(instance):
+def readfile(filename):
+    file = open(filename, 'r')
+    f = file.readlines()
+    
+    rectangles={}
+    i=0
+    
+    for line in f:
+        if i==0:
+           width_board = int(line)
+           rectangles["w"] = int(line)
+        elif i==1:
+            num_fig = int(line)
+            rectangles["n"] = int(line)
+        else:
+            line=line.strip()
+            line=line.split(' ')
+            rect=[ int(line[0]), int(line[-1])]
+            rectangles["r"+str(i)] = rect
+        i=i+1
+        
+    return rectangles
+
+def ConvertToDzn(instance, data):
     file = open("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances/"+ instance, "w+")
-    #her må vi prøve å lese filene i instances og konvertere til dictionaries som vi så konverterer til dzn
-    #data = {'a': [2,3], 'b': {4, 6}, 'c': {1, 2, 3}, 'd': {3: 4.5, 4: 1.3}, 'e': [[1, 2], [3, 4], [5, 6]]}
-    #pymzn.dict2dzn(data, fout='/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances/'+instance)
+    pymzn.dict2dzn(data, fout='/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances/'+instance)
     file.close()
 
 initial_count = 0
+data=[]
+
 for path in pathlib.Path("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/instances").iterdir():
-    if path.is_file():
-        initial_count += 1
+    #if path.is_file():
+     #   initial_count += 1
+    data.append(path)
 
-print(initial_count)
 
 
-#for i in range(4):
-#   ConvertToDzn("ins"+str(i+1)+".dzn")
+for i in range(len(data)):
+   ConvertToDzn("ins"+str(i+1)+".dzn", readfile(data[i]))
+   print(data[i])
--- readfile.py
+++ readfile.py
@@ -3,19 +3,24 @@ def readfile(filename):
     file = open(filename, 'r')
     f = file.readlines()
     
-    rectangles=[]
+    rectangles={}
     i=0
     
     for line in f:
         if i==0:
            width_board = int(line)
+           rectangles["w"] = int(line)
         elif i==1:
             num_fig = int(line)
+            rectangles["n"] = int(line)
         else:
             line=line.strip()
             line=line.split(' ')
             rect=[ int(line[0]), int(line[-1])]
-            rectangles.append(rect) 
+            rectangles["r"+str(i)] = rect
         i=i+1
         
-    return rectangles, num_fig, width_board
+    return rectangles
+
+
+print(readfile("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/instances/ins-1.txt"))
