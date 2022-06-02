6376dadf7a9ddac853151c203b6147e33ec767f9
Maud <johansson.maud98@gmail.com>
2022-06-02 11:48:44 +0200

    added new instances

--- ConvertingToDzn.py
+++ ConvertingToDzn.py
@@ -4,8 +4,8 @@ import pathlib;
 def readfile(filename):
     file = open(filename, 'r')
     f = file.readlines()
-    
     rectangles={"dx":[], "dy":[]}
+    geost ={"rect_size":"|", "rect_offset": "|", "shape": [], "valid_shapes": []}
     i=0
     
     for line in f:
@@ -13,6 +13,12 @@ def readfile(filename):
            rectangles["w"] = int(line)
         elif i==1:
             rectangles["n"] = int(line)
+            k = 0
+            for j in range(int(line)):
+                geost["rect_offset"]+= "0,0 |"
+                geost["shape"].append({j+1})
+                geost["valid_shapes"].append("{" + str(j+1 + k) +"," + str(j+2 + k) +"}")
+                k +=1
         else:
             line=line.strip()
             line=line.split(' ')
@@ -20,27 +26,33 @@ def readfile(filename):
             recty= int(line[-1])
             rectangles["dx"].append(rectx)
             rectangles["dy"].append(recty)
+            geost["rect_size"] += str(rectx) + "," + str(recty) + "|"
         i=i+1
+    geost["rect_offset"] = [geost["rect_offset"]]
+    geost["rect_size"] = [geost["rect_size"]]
         
-    return rectangles
+    return rectangles, geost
 
+rectangles, geost = readfile("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/instances/ins-2.txt")
 print(readfile("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/instances/ins-2.txt"))
 
-def ConvertToDzn(instance, data):
-    file = open("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances/"+ instance, "w+")
-    pymzn.dict2dzn(data, fout='/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances/'+instance)
-    file.close()
+def ConvertToDzn(instance, data, type = None):
+    if(type=="rectangles"):
+        file = open("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances/"+ instance, "w+")
+        pymzn.dict2dzn(data, fout='/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances/'+instance)
+        file.close()
+    else:
+        file = open("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances2/"+ instance, "w+")
+        pymzn.dict2dzn(data, fout='/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/newInstances2/'+instance)
+        file.close()
 
-initial_count = 0
 data=[]
 
 for path in pathlib.Path("/Users/maudjohansson/Combinatorial/Project/CombinatorialDecisionMaking/instances").iterdir():
-    if path.is_file():
-        initial_count += 1
     data.append(path)
 
-
-
 for i in range(len(data)):
-   ConvertToDzn("ins"+str(i+1)+".dzn", readfile(data[i]))
-   print(data[i])
+    rectangles, geost = readfile(data[i])
+    #ConvertToDzn("ins"+str(i+1)+".dzn", rectangles, "rectangles")
+    ConvertToDzn("ins"+str(i+1)+".dzn", geost)
+    print(data[i])
--- SAT/VisualizationResult/visualization.py
+++ SAT/VisualizationResult/visualization.py
@@ -1,21 +1,37 @@
+from stringprep import c8_set
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
-cmap = colors.ListedColormap(['peachpuff','pink','lightblue','thistle','lemonchiffon', 'peachpuff','pink','lightblue','thistle','lemonchiffon', 'peachpuff','pink','lightblue','thistle','lemonchiffon','peachpuff','pink','lightblue','thistle','lemonchiffon','peachpuff','pink','lightblue','thistle','lemonchiffon'])
+cmap = colors.ListedColormap(['peachpuff','pink','lightblue','thistle','lemonchiffon', 'peachpuff','pink','lightblue','thistle','lemonchiffon', 'peachpuff','pink','lightblue','thistle','lemonchiffon','peachpuff','pink','lightblue','thistle','lemonchiffon'])
 
 
-w=10
-l=11
-n=6
-c1=[3,7,3,3]
-c2=[0,7,3,4]
-c3=[6,6,3,5]
-c4=[6,0,3,6]
-c5=[3,0,3,7]
-c6=[0,0,3,7]
+w=30
+l=30
+n=20
+c1=[3,3,27,0]
+c2=[3,4,21,7]
+c3=[3,5,0,25]
+c4=[3,6,24,24]
+c5=[3,7,17,0]
+c6=[3,8,27,13]
+c7=[3,9,27,21]
+c8=[3,10,27,3]
+c9=[3,11,24,0]
+c10=[3,13,24,11]
+c11=[3,14,18,16]
+c12=[3,16,14,0]
+c13=[3,19,21,11]
+c14=[3,25,0,0]
+c15=[4,3,20,0]
+c16=[4,4,20,3]
+c17=[4,6,14,24]
+c18=[4,8,14,16]
+c19=[4,9,17,7]
+c20=[11,30,3,0]
 
 
-solution=[w,l,n,c1,c2,c3,c4]
+
+solution=[w,l,n,c1,c2,c3,c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20]
 
 
 def makeSolutionCoordinates(solution):
