1c8deb11fd38e99eedd7c69ff7d75ecc38d2901e
ragnakveli <91129201+ragnakveli@users.noreply.github.com>
2022-07-11 14:00:56 +0200

    Update placeCircuits2Rotation.py

--- MIP/placeCircuits2Rotation.py
+++ MIP/placeCircuits2Rotation.py
@@ -11,15 +11,8 @@ dy = [3, 4, 5, 6, 7, 8, 9, 3]
 
 # Changing boundary conditions as rotation is possible
 def placeRectWithRot(n,w,dx,dy):
-    #maxHeight = sum(dy);
-    #minHeight = max(dy);
-    findMaxHeight=np.zeros(n)
-    for c in range(n):
-        if dx[c]>dy[c]:
-            findMaxHeight[c]=dx[c]
-        else: 
-            findMaxHeight[c]=dy[c]
-    maxHeight = sum(findMaxHeight)   
+    maxHeight = sum(dy);
+    #minHeight = max(dy);  
     minHeight = min(min(dy),min(dx))
 
     (Circuits, X, Y) = (range(n), range(len(dx)), range(len(dy)))
